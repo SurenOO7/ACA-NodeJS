9d85d165e5a9a683956cca7bd99195ef3fdc9f13
Suren <arzsuren@gmail.com>
2022-01-29 16:33:08 +0400

    Homework_5_Avangers_Modified

--- JS_Task_5 _Avangers/JS_Task_5_Avengers.mjs
+++ JS_Task_5 _Avangers/JS_Task_5_Avengers.mjs
@@ -1,8 +1,19 @@
 import names from "./names.mjs"
 
-const team = ["villians", "heroes"]
-const PlayerNumberInEachTeam = 10;
+const team = ["villians", "heroes"] // as many groups as you want
+const PlayerNumberInEachTeam = 10; // as many players as you want
 let setIntervals = [];
+
+const hasWon = (team, WarriorsTeamNumber, Winners) => {
+    setIntervals.forEach(elem => {
+        clearInterval(elem)
+    })
+    WarriorsTeamNumber.forEach(elem => {
+        Winners.push(elem.name + "[" + elem.health + "]")
+    })
+    console.log(team[0], " have won")
+    console.log(Winners)
+}
 
 class Heroes {
     giveName = () => {
@@ -22,7 +33,7 @@ class Heroes {
         this.name=this.giveName();
         this.speed=this.giveSpeed();
         this.health=100;
-        side !== team[0] ? this.power=this.givePower()+5 : this.power=this.givePower()+7;
+        this.power=this.givePower();
         this.setIntervalID = 0;
     }
 }
@@ -52,37 +63,33 @@ const LetsTheGameBegin = () =>{
     for(let teamNumber=0; teamNumber<team.length; teamNumber++){
         for(let playerNumber=0; playerNumber<Warriors[teamNumber].length; playerNumber++){
             let setIntervalID = setInterval(() => {
+                if(!Warriors[teamNumber]){
+                    while(!Warriors[teamNumber]){
+                        teamNumber -=1
+                    }
+                }
                 if(!Warriors[teamNumber][playerNumber]){
                     if(team.length === 1){
-                        setIntervals.forEach(elem => {
-                            clearInterval(elem)
-                        })
-                        Warriors[teamNumber].forEach(elem => {
-                            Winners.push(elem.name + "[" + elem.health + "]")
-                        })
-                        console.log(team[0], " have won")
-                        console.log(Winners)
+                        hasWon(team, Warriors[teamNumber], Winners)
                         return
                     }
                     else{
-                        while(!Warriors[teamNumber][playerNumber]){
+                        while(!Warriors[teamNumber][playerNumber]){         
                             playerNumber -=1
                         }
                     }
                 }
                 if(team.length === 1){
-                    setIntervals.forEach(elem => {
-                        clearInterval(elem)
-                    })
-                    Warriors[teamNumber].forEach(elem => {
-                        Winners.push(elem.name + "[" + elem.health + "]")
-                    })
-                    console.log(team[0], " have won")
-                    console.log(Winners)
+                    hasWon(team, Warriors[teamNumber], Winners)
                     return
                 }
                 let theVictimTeam = chooseTheVictim(team.length)
-                while(Warriors[theVictimTeam].length === 0 || Warriors[theVictimTeam][0].team === Warriors[teamNumber][playerNumber].team){
+
+                while(Warriors[theVictimTeam].length === 0 || team[theVictimTeam] === Warriors[teamNumber][playerNumber].team){
+                    if(team.length === 2 && team[theVictimTeam] === Warriors[teamNumber][playerNumber].team){ // for optimization
+                        theVictimTeam === 1 ? theVictimTeam = 0 : theVictimTeam = 1
+                        break
+                    }
                     if(team.length === 1){
                         setIntervals.forEach(elem => {
                             clearInterval(elem)
@@ -102,21 +109,15 @@ const LetsTheGameBegin = () =>{
                         Warriors[theVictimTeam][theVictimPlayer].team, " with a power of ",
                         Warriors[teamNumber][playerNumber].power)
 
-                    if (Warriors[theVictimTeam][theVictimPlayer].health<0){
+                    if (Warriors[theVictimTeam][theVictimPlayer].health<=0){
                         clearInterval(Warriors[theVictimTeam][theVictimPlayer].setIntervalID)
                         console.log(Warriors[theVictimTeam][theVictimPlayer].name, " dies")
                         Warriors[theVictimTeam].splice(theVictimPlayer, 1)
+
                         if(Warriors[theVictimTeam].length === 0) {
+                            Warriors.splice(theVictimTeam, 1)     
                             team.splice(theVictimTeam, 1)           
                         }
-                    }
-                }
-                else{
-                    clearInterval(Warriors[theVictimTeam][theVictimPlayer].setIntervalID)
-                    console.log(Warriors[theVictimTeam][theVictimPlayer].name, " dies")
-                    Warriors[theVictimTeam].splice(theVictimPlayer, 1)
-                    if(Warriors[theVictimTeam].length === 0) {
-                        team.splice(theVictimTeam, 1)
                     }
                 }
             }, 1/Warriors[teamNumber][playerNumber].speed*200)
